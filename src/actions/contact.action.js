1668b74d5fc64e545ba7f5ff8221ec45be7945f2
Aurélien St <aurelien.strimelle@bstorm.be>
2026-09-03 13:39:51 +0200

    feat: check form

--- src/actions/contact.action.js
+++ src/actions/contact.action.js
@@ -1,4 +1,6 @@
 'use server'; // ← Directive pour m'assurer que les actions sont du "coté" serveur
+import { redirect } from 'next/navigation';
+import { z } from 'zod';
 
 // ! ↓ Code minimum pour une action utilisé avec le "useActionState"
 // export async function nomDeLaFonctionQuiFaitLeTraitement(state, formData) {
@@ -7,13 +9,38 @@
 // } 
 
 
-export async function handleContactSubmitAction(state, formData) {
+const contactDataSchema = z.object({
+    email: z.email({ error: 'L\'email n\'est pas valide' }),
+    msg: z.string({ error: 'Le message est requis' })
+        .trim()
+        .min(5, { error: 'Le message doit contenir minimum 5 caracteres' })
+});
 
-    console.log('Donnée recu !');
-    console.log(formData);
-    
+export async function handleContactSubmitAction(_state, formData) {
 
-    return state;
-} 
+    // Validation et conversion des données du formulaire
+    const { success, data, error } = await contactDataSchema.safeParseAsync(Object.fromEntries(formData));
+
+    // Gestion des erreurs du formulaires
+    if (!success) {
+        //? Renvoi un state avec les données du formulaire éronné et la liste d'erreurs
+        console.log(formData, error);
+        return {
+            formData,
+            error: z.flattenError(error)
+        };
+    }
+
+    // Traitement des données
+
+    // TODO Faire des intéractions avec : WebAPI, DB, Service web,...
+    console.log('Traitement du fomulaire de contact !!!');
+    console.log(data);
+
+    // Cloture du traitement possible :
+    // - Renvoi un state et actualise la page
+    // - Redirection vers une autre page
+    redirect('/');
+}
 
 
--- src/compenents/Contact/ContactForm.jsx
+++ src/compenents/Contact/ContactForm.jsx
@@ -16,14 +16,22 @@ export default function ContactForm() {
         <form action={handleSubmit}>
             <div>
                 <label htmlFor='input-email'>Email : </label>
-                <input name='email' id='input-email' type='email' className='border' />
+                <input name='email' id='input-email' type='text' className='border'
+                    defaultValue={state.formData?.get('email')}/>
+                { state.error?.fieldErrors?.email && (
+                    <span className='text-red-600'>{state.error.fieldErrors.email}</span>
+                )}
             </div>
             <div>
                 <label htmlFor='input-msg'>Message : </label>
-                <textarea name='msg' id='input-msg'  className='border' />
+                <textarea name='msg' id='input-msg'  className='border'
+                    defaultValue={state.formData?.get('msg')}/>
+                { state.error?.fieldErrors?.msg && (
+                    <span className='text-red-600'>{state.error.fieldErrors.msg}</span>
+                )}
             </div>
             <div>
-                <button type="submit">Envoyer</button>
+                <button type="submit" disabled={isPending}>Envoyer</button>
             </div>
         </form>
     )
